310991b239fe22a4a1b37d15179401ef17708b40
Shubham <shubham-gupta@outook.in>
2025-03-20 02:44:37 +0530

    Add login and registration pages with icon integration
    This commit adds new login and register pages with form layouts, toggles ESLint
    prop sorting rule, improves search input keyboard shortcut handling, adjusts
    layout padding, and integrates Iconify for icon components.

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,31 +1,59 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "@heroui/button";
 import { Input, Kbd } from "@heroui/react";
 import { SearchIcon } from "./icons";
+import { Link } from "@heroui/link";
+import { Icon } from "@iconify/react";
 
 export const SearchInput = () => {
   const [count, setCount] = useState(0);
 
+  React.useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      // Handle Command/Meta + K
+      if ((event.metaKey || event.ctrlKey) && event.key === "k") {
+        event.preventDefault();
+        document.getElementById("search-input")?.focus();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
+    <div className="flex flex-row items-center gap-2">
+      <Input
+        aria-label="Search"
+        id="search-input"
+        classNames={{
+          inputWrapper: "bg-default-100",
+          input: "text-sm",
+        }}
+        endContent={
+          <Kbd className="hidden lg:inline-block" keys={["command"]}>
+            K
+          </Kbd>
+        }
+        labelPlacement="outside"
+        placeholder="Search..."
+        startContent={
+          <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+        }
+        type="search"
+      />
+      <Button
+        as={Link}
+        isIconOnly
+        aria-label="Take a photo"
+        color="secondary"
+        variant="flat"
+      >
+        <Icon icon="fluent:triangle-right-48-filled" />
+      </Button>
+    </div>
   );
 };
